7a17e73631ef1a9ae47cc495f86772ede5b3f73e
elrizoos <77412223+elrizoos@users.noreply.github.com>
2024-04-07 22:27:30 +0200

    Añadida la estetica de apartado eliminar y editar paginas o secciones

---
+++ resources/views/admin/CONT-eliminarEditar.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.panel-control')
+
+@section('CONT-eliminarEditar')
+    <div class="contenedor-contenido seleccion">
+        <div class="contenedor-opciones-pagina">
+            <h2>PAGINAS</h2>
+            <div class="opcion opcion-eliminar">
+                <span></span>
+                <h3>ELIMINAR</h3>
+            </div>
+            <div class="opcion opcion-editar">
+                <span></span>
+                <h3>EDITAR</h3>
+            </div>
+        </div>
+        <div class="contenedor-opciones-seccion">
+            <h2>SECCIONES</h2>
+            <div class="opcion opcion-eliminar">
+                <span></span>
+                <h3>ELIMINAR</h3>
+            </div>
+            <div class="opcion opcion-editar">
+                <span></span>
+                <h3>EDITAR</h3>
+            </div>
+        </div>
+    </div>
+@endsection
